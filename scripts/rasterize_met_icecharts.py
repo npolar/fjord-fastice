113bc8d9c684ae3787bf3db51a198c889338c239
Mikhail Itkin <mikhail@npolar.no>
2017-03-21 13:49:34 +0100

    allow template filepath as optional argument

--- scripts/rasterize_met_icecharts.py
+++ scripts/rasterize_met_icecharts.py
@@ -15,20 +15,23 @@ def rasterize(convert_to_rast, out_rast, meta, field):
         # this is where we create a generator of geom, value pairs to use in rasterizing
         shapes = ((geom,value) for geom, value in zip(convert_to_rast.geometry, convert_to_rast[field]))
         burned = features.rasterize(shapes=shapes, fill=0, out=out_arr, transform=out.transform)
+        import ipdb; ipdb.set_trace()
         out.write_band(1, burned)       
                 
 if __name__ == '__main__':
 
     parser = argparse.ArgumentParser()
-    parser.add_argument("--ifile")
-    parser.add_argument("--ofile")
+    parser.add_argument("--ifile", "-i")
+    parser.add_argument("--ofile", "-o")
+    parser.add_argument("--template", "-t")
     args = parser.parse_args()
 
     ifile = args.ifile
     ofile = args.ofile
+    tif_template = args.template
 
     start = dt.now()
-    rst = rasterio.open('svalbard_template.tif')
+    rst = rasterio.open(tif_template)
     convert_to_rast = gpd.read_file(ifile) 
     convert_to_rast = convert_to_rast.loc[convert_to_rast['ICE_TYPE'] == 'Fast Ice']
     convert_to_rast['Junk'] = 1    
