d5a3f1b5d982a4ae0f6d9afe2cd48b77a03bacc8
Mikhail Itkin <mikhail@npolar.no>
2017-04-03 11:32:17 +0200

    Use metnocharts library for shape files processing

--- scripts/shp2nc.sh
+++ scripts/shp2nc.sh
@@ -1,15 +1,25 @@
 #!/bin/bash
+#
+# USAGE:
+# ./shp2nc.sh YEAR
+#
+# EXAMPLE:
+# ./shp2nc.sh 2015
+#
+# NOTES:
+# Assume that the original data is in $INDIR/icecharts-$YEAR
+# Requires Climate Data Operators (cdo) for nc files concatenation
+
 
 INDIR=/media/DataLocal/Projects/FjordIcePersistency/data
 BINDIR=/media/DataLocal/Projects/FjordIcePersistency/scripts
-TEMPLATEFILE=/media/DataLocal/Projects/FjordIcePersistency/templates/svalbard_template.tif
 
 function shp2nc {
     YEAR=$1
     for SHPFILE in ${INDIR}/icecharts-${YEAR}/ice${YEAR}0[45678]??.shp; do
         NCFILE=${SHPFILE%.*}.nc
 	echo "$(date): Converting $SHPFILE --> $NCFILE"
-	python -W ignore ${BINDIR}/rasterize_met_icecharts.py -i $SHPFILE -o $NCFILE -t $TEMPLATEFILE
+	python -W ignore ${BINDIR}/rasterize_fastice.py -i $SHPFILE -o $NCFILE
     done
     OUTFILE=${INDIR}/icecharts-${YEAR}/merged-icecharts-${YEAR}.nc
     rm $OUTFILE
